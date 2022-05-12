2501ca2508671fbc473cfd8157058185de0e23b1
Marcus Fredriksson <marcus@marcusfredriksson.com>
2022-05-12 21:49:45 +0200

    Reduced hover color effect for generated colors

--- src/utils/getHoverColorFromHex.ts
+++ src/utils/getHoverColorFromHex.ts
@@ -4,7 +4,7 @@ import isHex from "./isHex";
 const getHoverColorFromHex = (hex: string): string => {
   if (!isHex(hex)) return "#000000";
   const hsl = hexToHsl(hex);
-  hsl.l = hsl.l > 10 ? hsl.l - 10 : 0;
+  hsl.l = hsl.l > 5 ? hsl.l - 5 : 0;
   return `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`;
 };
 
